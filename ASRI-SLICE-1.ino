be5c9629fc27f225c26392404f082d98e5f48db3
GerhardVosloo <gerhardvosloo@icloud.com>
2024-11-23 08:54:03 +0200

    Add files via upload

---
+++ ASRI-SLICE-1.ino
@@ -0,0 +1,125 @@
+#define DATAFILE "/data.csv"
+//#ifndef I2C_SDA
+#define I2C_SDA 17//SDA
+//#endif
+//#ifndef I2C_SCL
+#define I2C_SCL 18//SCL
+//#endif
+
+// expansion port pins for SD Card
+#define MMC_CLK 36
+#define MMC_CMD 37
+#define MMC_DAT0 38
+#define MMC_DAT1 34
+#define MMC_DAT2 33
+#define MMC_DAT3 35
+
+// general libraries
+#include <ArduinoJson.h>
+#include <Wire.h>
+
+// libraries for SD card
+#include "FS.h"
+#include "SD_MMC.h"
+
+// libraries for sensors
+#include <IGA.h> //air quality
+IGA iga;
+
+#include <IIA.h> //accelerometer
+IIA iia;
+
+#include <IWB.h> //barometer
+IWB iwb;
+
+void setup() {
+  Serial.begin(115200);
+  
+  // init xChips
+  Wire.setPins(I2C_SDA, I2C_SCL);
+  Wire.begin();
+
+  if (iga.begin()) {
+        Serial.println("IGA initialized successfully.");
+    } else {
+        Serial.println("Failed to initialize IGA");
+        exit(0);
+    }
+
+  if (iia.begin()) {
+        Serial.println("IIA initialized successfully.");
+    } else {
+        Serial.println("Failed to initialize IIA");
+        exit(0);
+    }
+
+
+  if (iwb.begin()) {
+        Serial.println("IWB initialized successfully.");
+    } else {
+        Serial.println("Failed to initialize IWB");
+        exit(0);
+    }
+
+  //init SD Card
+  if (!SD_MMC.setPins(MMC_CLK, MMC_CMD, MMC_DAT0, MMC_DAT1, MMC_DAT2, MMC_DAT3)) {
+    Serial.println("Pin change failed!");
+    exit(0);
+  }
+
+  if (!SD_MMC.begin()) {
+    Serial.println("Card Mount Failed.");
+    exit(0);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char buffer[200];
+
+  // Start with milli seconds in the buffer
+  sprintf(buffer, "%ld", millis());
+
+  // add air quality data to buffer
+  uint16_t co2ppm, tvocppb;
+  if (iga.getData(co2ppm, tvocppb)) {
+    sprintf(buffer, "%s,%d,%d", buffer, co2ppm, tvocppb);
+  } else {
+    Serial.println("Failed to get IGA data");
+  }
+
+  // add accelerometer data to buffer
+  float x, y, z;
+  if (iia.getData(x, y, z)) {
+    sprintf(buffer, "%s,%.4f,%.4f,%.4f", buffer, x, y, z);
+  } else {
+    Serial.println("Failed to get IIA data");
+  }
+  
+  // add barometer data to buffer
+  double hPa, celsius;
+  if (iwb.getData(hPa, celsius)) {
+    sprintf(buffer, "%s,%.4f,%.4f", buffer, hPa, celsius);
+  } else {
+    Serial.println("Failed to get IWB data");
+  }
+
+  // Write data to SD Card:
+  // Open file...
+  File file = SD_MMC.open(DATAFILE, FILE_APPEND);  // Open file with filename defined at the top. Open for appending, meaning it creates a file if it doesn't exist and add data to the end of the file
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    exit(0);
+  }
+  // Write to file...
+  if (!file.println(buffer)) {
+    Serial.println("Append failed");
+    exit(0);
+  }
+  // Close file...
+  file.close();
+
+  // Print the data to the Serial Port
+  // The CSV data format makes it viable to see live data using Microsoft Datastreamer (an Excel function): https://support.microsoft.com/en-us/office/what-is-data-streamer-1d52ffce-261c-4d7b-8017-89e8ee2b806f
+  Serial.println(buffer);
+}
